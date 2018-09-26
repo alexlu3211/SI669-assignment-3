5a88a7ba003212f167a88e57e15805e0d9886575
Alex Lu <alexlu@alexs-mbp.attlocal.net>
2018-09-26 01:47:10 -0400

    first commit

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Mr. Nice', img: '/assets/images/01.jpg' },
+  { id: 12, name: 'Narco', img: '/assets/images/02.jpg'},
+  { id: 13, name: 'Bombasto', img: '/assets/images/03.jpg'},
+  { id: 14, name: 'Celeritas', img: '/assets/images/04.jpg' },
+  { id: 15, name: 'Magneta', img: '/assets/images/05.jpg' },
+  { id: 16, name: 'RubberMan', img: '/assets/images/06.jpg' },
+  { id: 17, name: 'Dynama', img: '/assets/images/07.jpg' },
+  { id: 18, name: 'Dr IQ', img: '/assets/images/08.jpg' },
+  { id: 19, name: 'Magma', img: '/assets/images/09.jpg' },
+  { id: 20, name: 'Tornado', img: '/assets/images/10.jpg' }
+];
